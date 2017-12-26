94467c1f00d95dfa9cb7dc9183e097011a1f06fb
tracer-pa2017 <tracer@njuics.org>
2017-12-26 07:27:23 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  07:27:23 up 1 day,  3:01,  1 user,  load average: 0.00, 0.03, 0.05 e089f63b5211351598f8c66891cff2ac4eeaf1a2

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -20,8 +20,11 @@ typedef union {
 }CR0;
 typedef union{
 	struct{
+		uint32_t pad0 :3;
+		uint32_t page_write_through :1;
+		uint32_t page_cache_disable :1;
+		uint32_t pad1 :7;
 		uint32_t page_directory_base :20;
-		uint32_t reserve:12;
 	};
 	uint32_t val;
 }CR3;
