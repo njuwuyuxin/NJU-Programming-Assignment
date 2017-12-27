477ba56a0d8b5e87a64f38ebf57f21837e56d13b
tracer-pa2017 <tracer@njuics.org>
2017-12-27 11:14:40 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  11:14:40 up 1 day,  9:57,  1 user,  load average: 0.11, 0.14, 0.10 b893871d77ea8fa5e40d8b4d11163319fab5bbe

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -55,6 +55,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 }
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
+	paddr_t paddr=laddr;
 #ifdef IA32_PAGE
 	if(cpu.cr0.pg==1)
 		paddr=page_translate(laddr);
