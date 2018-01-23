c2152837201c299c24df3f161ab5d9d434508bd0
tracer-pa2017 <tracer@njuics.org>
2018-01-23 05:53:10 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  05:53:10 up 2 days, 14 min,  1 user,  load average: 0.35, 0.28, 0.16 4739ab46db6af401d0e6c22688d8ab67742654ef

--- nemu/src/cpu/instr/idt.c
+++ nemu/src/cpu/instr/idt.c
@@ -4,3 +4,22 @@ make_instr_func(cli)
 	cpu.eflags.IF=0;
 	return 1;
 }
+make_instr_func(lidt)
+{
+	int len=1;
+	len+=modrm_rm(eip+1,&opr_src);
+	//printf("addr=%x\t",opr_src.addr);
+	//printf("val=%x\t",opr_src.val);
+	opr_src.data_size=16;
+	operand_read(&opr_src);
+	cpu.idtr.limit=opr_src.val;
+	opr_src.addr=opr_src.addr+2;
+	opr_src.data_size=32;
+	operand_read(&opr_src);
+	cpu.idtr.base=opr_src.val;
+	//printf("limit=%x\t",cpu.gdtr.limit);
+	//printf("base=%x\t",cpu.gdtr.base);
+	return len;
+	//printf()
+
+}
