ae1864b2be6a69419a0072133eafac01ef6720d2
tracer-pa2017 <tracer@njuics.org>
2018-01-24 07:37:04 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  07:37:04 up 2 days,  8:00,  1 user,  load average: 0.45, 0.33, 0.26 2a5183306776c3c33a5e6d41e2bf02bc76e3531d

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -15,6 +15,27 @@ static void instr_execute_1op()
 	vaddr_write(cpu.esp,2,data_size/8,opr_src.val);
 }
 
+make_instr_func(pusha)
+{
+	uint32_t temp=cpu.esp;
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.eax);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.ecx);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.edx);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.ebx);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,temp);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.ebp);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.esi);
+	cpu.esp-=4;
+	vaddr_write(cpu.esp,2,4,cpu.edi);
+}
+
 make_instr_impl_1op(push,r,l)
 make_instr_impl_1op(push,rm,v)
 make_instr_impl_1op(push,i,b)
