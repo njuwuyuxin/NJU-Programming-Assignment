8be2047047f3d6ccd6c63e18fcbe18a8f4af3f47
tracer-pa2017 <tracer@njuics.org>
2018-01-24 07:30:44 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  07:30:44 up 2 days,  7:54,  1 user,  load average: 0.32, 0.25, 0.21 a58f6af1d7716abd6a2692d56eb145646fd5c1ab

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -14,9 +14,10 @@ void raise_intr(uint8_t intr_no) {
 	vaddr_write(cpu.esp,2,4,cpu.cs.val);
 	cpu.esp-=4;
 	vaddr_write(cpu.esp,2,4,cpu.eip);
+	cpu.IF=0;
+	cpu.TF=0;
 
-
-	printf("idtr=%x\n",cpu.idtr.base);
+	//printf("idtr=%x\n",cpu.idtr.base);
 	uint32_t base_addr=cpu.idtr.base;
 	uint32_t final_addr=base_addr+intr_no*8;
 	GateDesc Desc;
@@ -26,7 +27,11 @@ void raise_intr(uint8_t intr_no) {
 	uint32_t offset=Desc.offset_31_16;
 	offset=offset<<16;
 	offset+=Desc.offset_15_0;
-	printf("Desc.offset=%x\n",offset);
+	//printf("Desc.offset=%x\n",offset);
+	
+	cpu.cs.val=Desc.selector;
+	cpu.eip=offset;
+	
 #endif
 }
 
