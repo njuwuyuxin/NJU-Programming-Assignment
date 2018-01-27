07b2144629c2a3149d7c9561815b55602432b630
tracer-pa2017 <tracer@njuics.org>
2018-01-27 04:34:14 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  04:34:14 up  2:22,  1 user,  load average: 0.39, 0.44, 0.36 ba04965519e8242f9ef1161b1e74e2f9b78eb2b3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -49,10 +49,10 @@ uint32_t loader() {
 			uint32_t physical_addr=mm_malloc(addr,ph->p_memsz);
 			
 #ifdef HAS_DEVICE_IDE
-			assert(ph->p_filesz<=4096);
-			ide_read(buf,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
-			memcpy((void*)(physical_addr),(void*)(buf),ph->p_filesz);
-			//memcpy((void*)(physical_addr),(void*)((void*)elf+ph->p_offset),ph->p_filesz);
+			//assert(ph->p_filesz<=4096);
+			//ide_read(buf,ELF_OFFSET_IN_DISK+ph->p_offset,ph->p_filesz);
+			//memcpy((void*)(physical_addr),(void*)(buf),ph->p_filesz);
+			memcpy((void*)(physical_addr),(void*)((void*)elf+ph->p_offset),ph->p_filesz);
 #else
 			memcpy((void*)(physical_addr),(void*)((void*)elf+ph->p_offset),ph->p_filesz);
 #endif
