3acd91b0d1bfd3541dfd1a3cc1d095f330f1602a
tracer-pa2017 <tracer@njuics.org>
2018-01-27 04:34:44 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  04:34:44 up  2:23,  1 user,  load average: 0.43, 0.45, 0.36 94a172146cac3be9c0e179f8167e14089527eb1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -18,8 +18,8 @@ uint32_t loader() {
 	Elf32_Phdr *ph, *eph;
 
 #ifdef HAS_DEVICE_IDE
-	uint8_t buf[4096];
-	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	uint8_t buf[65536];
+	ide_read(buf, ELF_OFFSET_IN_DISK, 65536);
 	elf = (void*)buf;
 	Log("ELF loading from hard disk.");
 #else
