2c70b5bf1b98f254608fc914ec1d61204bf17ae1
tracer-pa2017 <tracer@njuics.org>
2018-01-27 03:15:31 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  03:15:31 up  1:04,  1 user,  load average: 1.20, 0.57, 0.37 abd6b0c9c80aac7abd73e50913e92d53a5e37f5a

--- nemu/src/cpu/instr/in.c
+++ nemu/src/cpu/instr/in.c
@@ -2,7 +2,7 @@
 #include "device/port_io.h"
 make_instr_func(IN_b)
 {
-	cpu.gpr[0]._8[1]=pio_read(cpu.gpr[2]._16,1);
+	cpu.gpr[0]._8[0]=pio_read(cpu.gpr[2]._16,1);
 	//printf("eip=%x\n",cpu.eip);
 	return 1;
 }
