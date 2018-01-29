a6f3dda5506e05700704a67de01fb9d4a5b1c3f8
tracer-pa2017 <tracer@njuics.org>
2018-01-29 08:56:32 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  08:56:32 up  8:21,  1 user,  load average: 0.04, 0.03, 0.00 547eae5471cc6343fa99510e22ca6636675f9082

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -15,7 +15,7 @@ void create_video_mapping() {
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	assert(0);
+	//assert(0);
 	//panic("please implement me");
 }
 
