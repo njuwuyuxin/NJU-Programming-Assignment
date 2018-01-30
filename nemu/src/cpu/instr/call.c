d6a18898ea01e769b1da27bbcf6c542a7609292d
tracer-pa2017 <tracer@njuics.org>
2018-01-30 03:30:10 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  03:30:10 up 14:38,  1 user,  load average: 0.19, 0.15, 0.06 5b8b1531980f933e7ebf05b28a553fb437753bf

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,6 +3,7 @@
 
 make_instr_func(call_rm_v)
 {
+	/*
 	int len=1;
 	decode_data_size_v
 	decode_operand_rm
@@ -18,6 +19,23 @@ make_instr_func(call_rm_v)
 
 	int offset=sign_ext(opr_src.val,opr_src.data_size);
 	cpu.eip=offset;
+	return 0;
+	*/
+	int len=1;
+	opr_src.data_size=data_size;
+	len+=modrm_rm(eip+1,&opr_src);
+	cpu.esp-=data_size/8;
+	uint32_t data=cpu.eip+len;
+	vaddr_write(cpu.esp,SREG_SS,data_size/8,data);
+	operand_read(&opr_src);
+	print_asm_1("call"," *",len,&opr_src);
+	if(data_size==16)
+		cpu.eip=opr_src.val&0xffff;
+	else
+	{
+		int offset=sign_ext(opr_src.val,data_size);
+		cpu.eip=offset;
+	}
 	return 0;
 }
 make_instr_func(call)
