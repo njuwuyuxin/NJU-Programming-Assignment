f62258115febef8a2de698da4c2710f03d32a639
tracer-pa2017 <tracer@njuics.org>
2018-01-30 01:22:25 -0500

    >  nemu 161290019 wuyuxin Linux debian 4.9.0-3-686-pae #1 SMP Debian 4.9.30-2+deb9u2 (2017-06-26) i686 GNU/Linux  01:22:25 up 12:30,  1 user,  load average: 0.39, 0.37, 0.34 e76e1c9a9c2566804dee72fcb50d8c826443df44

--- game/src/typing/game.c
+++ game/src/typing/game.c
@@ -59,11 +59,11 @@ main_loop(void) {
 		 * 从而主循环中维护的时钟可能与实际时钟相差较多。为了维持游戏的正常运行，必须补上
 		 * 期间错过的每一帧游戏逻辑。 */
 		while (now < target) { 
-	assert(0);
 			/* 每隔一定时间产生一个新的字符 */
 			if (now % (HZ / CHARACTER_PER_SECOND) == 0) {
 				create_new_letter();
 			} 
+	assert(0);
 			/* 每隔一定时间更新屏幕上字符的位置 */
 			if (now % (HZ / UPDATE_PER_SECOND) == 0) {
 				update_letter_pos();
